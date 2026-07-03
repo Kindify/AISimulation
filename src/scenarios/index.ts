4474ada2972f5b7926a751208979257318f494bc
Kindify <matthewdjanes@gmail.com>
2026-07-03 18:19:18 -0400

    Update index.ts

--- src/scenarios/index.ts
+++ src/scenarios/index.ts
@@ -5,8 +5,9 @@ import theVoid from './the-void.json';
 import theJump from './the-jump.json';
 import theDayOf from './the-day-of.json';
 import sovereignDnaMandate from './sovereign-dna-mandate.json';
+import exportControlPrecedent from './export-control-precedent.json';
 
 export const CORE_SCENARIOS = [syntheticCandidate, invisibleEpidemic];
 export const WORKSHOP_SCENARIOS = [theCrossing, theVoid, theJump, theDayOf];
-export const TOPICAL_SCENARIOS = [sovereignDnaMandate];
+export const TOPICAL_SCENARIOS = [sovereignDnaMandate, exportControlPrecedent];
 export const ALL_SCENARIOS = [...CORE_SCENARIOS, ...WORKSHOP_SCENARIOS, ...TOPICAL_SCENARIOS];
